0493fbe4bcd03554591faa23e1088818ce44a9c0
Yann Mainier <yann.mainier@gmail.com>
2025-11-14 22:05:30 +0000

    add tools

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,8 +1,21 @@
 import { openai } from "@ai-sdk/openai";
-import { convertToModelMessages, streamText, UIMessage } from "ai";
+import { convertToModelMessages, streamText, UIMessage, tool, stepCountIs } from "ai";
+import { z } from "zod";
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
+
+export const weather = tool({
+  description: "Get the weather in a location",
+  inputSchema: z.object({
+    location: z.string().describe("The location to get the weather for"),
+  }),
+  // location below is inferred to be a string:
+  execute: async ({ location }) => ({
+    location,
+    temperature: 72 + Math.floor(Math.random() * 21) - 10,
+  }),
+});
 
 export async function POST(req: Request) {
   const { messages }: { messages: UIMessage[] } = await req.json();
@@ -12,6 +25,8 @@ export async function POST(req: Request) {
     system:
       "You are a helpful assistant named Chatty. answer using markdown unless asked otherwise.",
     messages: convertToModelMessages(messages),
+    tools: { weather },
+    stopWhen: stepCountIs(5),
   });
 
   return result.toUIMessageStreamResponse();
